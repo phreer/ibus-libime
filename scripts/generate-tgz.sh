acf3b9c2992a339db926c83f39d12d98a041497b
Weifeng Liu <weifeng.liu@intel.com>
2025-12-23 17:52:58 +0800

    scripts: Show tgz file generated

--- scripts/generate-tgz.sh
+++ scripts/generate-tgz.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+set -e
+
 VERSION=$(cat VERSION)
 git archive --format=tar.gz --prefix=ibus-libime-${VERSION}/ -o ~/rpmbuild/SOURCES/ibus-libime-${VERSION}.tar.gz HEAD
+echo "ibus-libime-${VERSION}.tar.gz is generated"
